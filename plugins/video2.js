8b9b22c5d10e4c7bc2a22a683f566dd115ad3580
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2025-01-12 16:31:25 +0530

    Create video2.js

---
+++ plugins/video2.js
@@ -0,0 +1,44 @@
+const config = require('../config');
+const { cmd } = require('../command');
+const { ytsearch, ytmp3, ytmp4 } = require('@dark-yasiya/yt-dl.js'); 
+
+
+cmd({
+    pattern: "video2",
+    alias: ["watch","ytvideo"],
+    react: "🎥",
+    desc: "Download Youtube video",
+    category: "download",
+    use: '.video2 < Yt url or Name >',
+    filename: __filename
+},
+async(conn, mek, m,{ from, prefix, quoted, q, reply }) => {
+try{
+
+if(!q) return await reply("Please give me Yt url or Name")
+	
+const yt = await ytsearch(q);
+if(yt.results.length < 1) return reply("Results is not found !")
+
+let yts = yt.results[0]  
+const ytdl = await ytmp3(yts.url)		
+let ytmsg = `🎥 NADEEN-MD VIDEO DOWNLOADER 🎥
+
+
+🎥 *TITLE :* ${yts.title}
+🤵 *AUTHOR :* ${yts.author.name}
+⏱ *RUNTIME :* ${yts.timestamp}
+👀 *VIEWS :* ${yts.views}
+🖇️ *URL :* ${yts.url}
+
+> *▫ NADEEN-MD*
+`
+await conn.sendMessage(from, { image: { url: yts.thumbnail || yts.image || '' }, caption: ytmsg }, { quoted: mek });
+await conn.sendMessage(from, { video: { url: ytdl.download.url }, mimetype: "video/mp4" }, { quoted: mek })
+await conn.sendMessage(from, { document: { url: ytdl.download.url }, mimetype: "video/mp4", fileName: ytdl.result.title + '.mp4', caption: `*©𝗡𝗔𝗗𝗘𝗘𝗡-𝗠𝗗*` }, { quoted: mek })
+
+} catch (e) {
+console.log(e)
+reply(e)
+}}
+)
