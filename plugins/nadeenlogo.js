cfc9617a7a3a78b43788c460b18c220d67bf41b9
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2025-01-12 15:31:23 +0530

    Update nadeenlogo.js

--- plugins/nadeenlogo.js
+++ plugins/nadeenlogo.js
@@ -1,1 +1,25 @@
-const _0xa7117e=_0x343f;(function(_0x458e40,_0x459848){const _0x165c42=_0x343f,_0xe66840=_0x458e40();while(!![]){try{const _0x4ae38b=parseInt(_0x165c42(0x17d))/0x1+-parseInt(_0x165c42(0x16c))/0x2+parseInt(_0x165c42(0x173))/0x3*(-parseInt(_0x165c42(0x16f))/0x4)+-parseInt(_0x165c42(0x170))/0x5*(parseInt(_0x165c42(0x16e))/0x6)+parseInt(_0x165c42(0x16a))/0x7*(-parseInt(_0x165c42(0x179))/0x8)+parseInt(_0x165c42(0x169))/0x9*(parseInt(_0x165c42(0x17c))/0xa)+parseInt(_0x165c42(0x17a))/0xb*(parseInt(_0x165c42(0x174))/0xc);if(_0x4ae38b===_0x459848)break;else _0xe66840['push'](_0xe66840['shift']());}catch(_0x56a8c3){_0xe66840['push'](_0xe66840['shift']());}}}(_0x45f6,0x66744));function _0x343f(_0x27e38f,_0x2e43f8){const _0x45f613=_0x45f6();return _0x343f=function(_0x343f90,_0x588614){_0x343f90=_0x343f90-0x169;let _0x147af5=_0x45f613[_0x343f90];return _0x147af5;},_0x343f(_0x27e38f,_0x2e43f8);}function _0x45f6(){const _0x1c580f=['1245981qJuBaZ','https://files.catbox.moe/2nmi9q.png','250WhyiNe','565817fxqdxi','main','112851kKCiKe','3122406ESLSgh','https://telegra.ph/file/f2a6ee1ab649a2fb72708.jpg','1256492LWBWJE','https://i.imgur.com/AzQrKon.png','258NatVzx','16dgTjTe','16420faZHdK','nadeenlogo','_0x52dd92','112719ACGXCR','96iiIMRp','log','❮❮\x20NADEEN-MD\x20WALLPEPAR\x20👾💗\x20❯❯','https://telegra.ph/file/c7ce95554df8fcfa85680.jpg','🎗️','8DlgkJc'];_0x45f6=function(){return _0x1c580f;};return _0x45f6();}const {_0x2f7afd:cmd,_0xae6b18:commands}=require('../command');cmd({'pattern':_0xa7117e(0x171),'_0x333cac':'wallpaper\x20the\x20bot','category':_0xa7117e(0x17e),'_0x22d5ff':_0xa7117e(0x178),'filename':__filename},async(_0x26c92b,_0x22d6dd,_0x3301ee,{from:_0x53aebb,_0x19ec65:_0x392876,body:_0x357f3e,_0x30773f:_0x567570,command:_0x3dc9d5,_0x12bb1c:_0x29915b,_0x273f3d:_0x2f869f,_0x39c083:_0x27b430,sender:_0xa83f9,_0x10a5e1:_0x152aff,_0x2b5374:_0x429086,_0x2ccf1c:_0x4dc275,_0x26e5dc:_0x4c08e6,_0x3707a8:_0xa1e8e6,_0x5e0262:_0x567fbf,_0x4e8a89:_0x30e8ae,_0x5d0100:_0x48dbda,participants:_0x18357,_0x488fe8:_0x415348,_0xbba65b:_0xf1f35d,_0x4d302e:_0x30ddb4,_0x3772cc:_0x385876})=>{const _0x4c02d3=_0xa7117e;try{let _0x4ba18f=_0x4c02d3(0x176);await _0x26c92b[_0x4c02d3(0x172)](_0x53aebb,{'_0x4e5aff':{'url':_0x4c02d3(0x177)},'caption':_0x4ba18f},{'_0x19ec65':_0x22d6dd}),await _0x26c92b[_0x4c02d3(0x172)](_0x53aebb,{'_0x4e5aff':{'url':'https://telegra.ph/file/c253ba5258dc2c5c2f33d.png'},'caption':_0x4ba18f},{'_0x19ec65':_0x22d6dd}),await _0x26c92b[_0x4c02d3(0x172)](_0x53aebb,{'_0x4e5aff':{'url':_0x4c02d3(0x16b)},'caption':_0x4ba18f},{'_0x19ec65':_0x22d6dd}),await _0x26c92b[_0x4c02d3(0x172)](_0x53aebb,{'_0x4e5aff':{'url':_0x4c02d3(0x16d)},'caption':_0x4ba18f},{'_0x19ec65':_0x22d6dd}),await _0x26c92b['_0x52dd92'](_0x53aebb,{'_0x4e5aff':{'url':_0x4c02d3(0x17b)},'caption':_0x4ba18f},{'_0x19ec65':_0x22d6dd});}catch(_0x1f34bd){console[_0x4c02d3(0x175)](_0x1f34bd),_0x385876(''+_0x1f34bd);}});
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "nadeenlogo",
+    desc: "wallpaper the bot",
+    category: "main",
+    react: "🎗️",
+    filename: __filename
+},
+
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let dec = `❮❮ NADEEN-MD WALLPEPAR 👾💗 ❯❯`
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/c7ce95554df8fcfa85680.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/c253ba5258dc2c5c2f33d.png`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/f2a6ee1ab649a2fb72708.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://i.imgur.com/AzQrKon.png`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://files.catbox.moe/2nmi9q.png`},caption:dec},{quoted:mek});
+    
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
