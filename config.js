94de130c61a861ce66dd2a844e054c1f83565ed3
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2025-01-14 13:54:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "", //put your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 MODE: process.env.MODE || "private", //private or public
+SUDO:"94711451319", //deleted messege recovory number
 AUTO_VOICE:"true", //true or false
 INBOX_BLOCK:"false",
 ANTI_DELETE:"true",
