231b8fbebc67b88619c971054e6c552a681b74aa
grbmsirithilaka2009 <139479812+grbmsirithilaka2009@users.noreply.github.com>
2025-01-16 16:40:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put your session id
+SESSION_ID: process.env.SESSION_ID || "cxwXhQAT#SnPT6lg8hwv9RCBI_xYWUKcWnv_zBoyG1rxAD77MdaA", //put your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 MODE: process.env.MODE || "private", //private or public
 AUTO_VOICE:"true" //true or false
